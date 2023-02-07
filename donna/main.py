a753a1d213ce62b441557c2f3cc84d0de9f4f46e
elroyD <72175303+elroydo@users.noreply.github.com>
2023-02-07 20:27:43 +0000

    feat: window (canvas) interactivity
     - close app by clicking anywhere inside window (canvas)

--- donna/main.py
+++ donna/main.py
@@ -12,13 +12,20 @@ def move_point():
         if canvas.coords(point)[2] > 800: #check coordinates
             direction = 'right'
     canvas.after(50, move_point) #update canvals every 50ms
+    
+#close window by pressing anywhere within window (canvas)
+def close_app(event):
+    print(event)
+    root.destroy()
 
 root = Tk()
 root.title("Belladonna")
 
 #create canvas window
 canvas = Canvas(root, width=800, height=800)
-canvas.pack() #organise elements (widgets) 
+canvas.pack() #organise elements (widgets)
+
+canvas.bind("<Button-1>", close_app) #make canvas button
 
 #create a dot
 point = canvas.create_oval(390, 390, 400, 400, fill="blue")
