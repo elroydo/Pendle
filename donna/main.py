5dac5bc7c3e749d1defb1b13d4b4dc5322ea082c
elroy.D <72175303+elroydo@users.noreply.github.com>
2023-02-28 20:34:12 +0000

    feat: timer cycles
     - added 30 second cycles for a total of 5 minutes
     - health monitoring begins and ends with session
     - thread termination flag
     - guidance messages with cycles
     - added another screen after session
     - fixed function looping issue: now terminates after canvas changes (function id)

--- donna/main.py
+++ donna/main.py
@@ -8,6 +8,8 @@ from tkinter import *
 def cardMonitor():
     print('Monitoring...')
 
+    global stopThread, elapsedTime
+
     #load Haar face and eye cascades (face shapes)
     faceCascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -134,31 +136,47 @@ def cardMonitor():
                 frame[y1:y2, x1:x2] = pyramid[levels] * 17
 
         cv.putText(frame, f'BPM: {bpmBuffer.mean()}', (10, 30), font, 0.6, (255, 255, 255), 1) #display bpm
-        cv.putText(frame, f'Emotion: {str(dominantEmotion)}', (10, 60), font, 0.6, (255, 255, 255), 1) #display emotion in frame
+        cv.putText(frame, f'BRPM: {bpmBuffer.mean()//4}', (10, 60), font, 0.6, (255, 255, 255), 1) #display brpm
+        cv.putText(frame, f'Emotion: {str(dominantEmotion)}', (10, 90), font, 0.6, (255, 255, 255), 1) #display emotion in frame
 
         #show frames in a window
         if frame is not None:
             cv.imshow('Janus', frame)
             cv.waitKey(1)
+
+        #termination check
+        if stopThread:
+            break
             
     #release capture on closing
     cap.release()
-    cv.destroyAllWindows()
+    cv.destroyAllWindows() #close all windows
     print("Terminating...")
 
 #Canvas jazz
 #point moves left to right repeatedly
 def movePoint():
-    global direction #access global variable
-    if direction == 'right':
-        canvas2.move(point, -10, 0) #move point 10 pixels to the left
-        if canvas2.coords(point)[0] < 30:  #check coordinates
-            direction = 'left' #set direction
+    global functionID, direction, startTime, elapsedTime #access global variables
+    elapsedTime = int(time.time() - startTime) #timer
+    if direction == 1:
+        canvas2.move(point, -12, 0) #move point 10 pixels to the left
+        if canvas2.coords(point)[0] < 30: #check coordinates
+            direction = 0 #set direction
     else:
-        canvas2.move(point, 10, 0) #move point 10 pixels to the right
+        canvas2.move(point, 12, 0) #move point 10 pixels to the right
         if canvas2.coords(point)[2] > 770: #check coordinates
-            direction = 'right'
-    canvas2.after(10, movePoint) #update canvals every 10ms
+            direction = 1
+
+    if elapsedTime > 0 and elapsedTime % 30 == 0: #show message every 30 seconds
+        canvas2.create_text(400, 300, text="Please take a deep breath", font=fontHeading, fill="black", tags="guideText") #create text
+    if elapsedTime % 40 >= 33: #delete message after 3 seconds
+        canvas2.delete("guideText") #removes old text
+    
+    if elapsedTime == 300: #5 minute timer
+        root.after_cancel(functionID) #stop function
+        showCanvas3() #display next screen
+    else:
+        functionID = canvas2.after(10, movePoint) #update canvas every 10ms
 
 #show and update time
 def updateTime():
@@ -177,14 +195,27 @@ def showCanvas1(event=None):
     helpButton.place(x=750, y=750) #button position on canvas
     
 def showCanvas2(event=None):
+    global startTime
+    startTime = time.time() #get current time
+    t1.start() #start thread
     canvas1.pack_forget()
     helpButton.pack_forget()
     canvas2.pack()
+    movePoint() #call function to move point
     
 def showCanvas3(event=None):
+    global stopThread
+    root.after_cancel(functionID) #stop function
+    stopThread = True #terminate thread
     canvas2.pack_forget()
     canvas3.pack()
-    canvas3.create_text(400, 400, text="Press here to close", font=fontHeading, fill="black")
+    canvas3.create_oval(380, 380, 400, 400, fill="black") #create static point in the centre
+    canvas3.create_text(400, 300, text="Focus on the point below and take a deep breath.", font=fontHeading, fill="black")
+
+def showCanvas4(event=None):
+    canvas3.pack_forget()
+    canvas4.pack()
+    canvas4.create_text(400, 400, text="Press here to close", font=fontHeading, fill="black")
     
 def showHelpCanvas(event=None):
     canvas1.pack_forget()
@@ -198,35 +229,46 @@ root.title("Pendle")
 root.geometry('800x800') #set geometry
 root.resizable(False, False)
 
+#create canvas windows
+height = 800
+width = 800
+canvas1 = Canvas(root, width=width, height=height)
+canvas2 = Canvas(root, width=width, height=height)
+canvas3 = Canvas(root, width=width, height=height)
+canvas4 = Canvas(root, width=width, height=height)
+helpCanvas = Canvas(root, width=width, height=height)
+
+#create canvas buttons
+canvas1.bind("<Button-1>", showCanvas2) #begin
+canvas2.bind("<Button-1>", showCanvas3) #go to end
+canvas3.bind("<Button-1>", showCanvas4) #go to end
+canvas4.bind("<Button-1>", lambda event: root.quit()) #close window
+helpCanvas.bind("<Button-1>", showCanvas1) #go to start
+
+#help button
+helpButton = Button(canvas1, text="Help", command=showHelpCanvas, relief=FLAT) #create button
+helpButton.config(image=PhotoImage(file='assets/icons/question.png')) #import icon
+
+#create a point
+x = width//2 #calculate x centre
+y = height//2 #calculate y centre
+radius = 10 #set point radius
+point = canvas2.create_oval(x - radius, y - radius, x + radius, y + radius, fill="black")
+direction = 0 #add direction
+
 #create font styles
 fontHeading = ("Arial", 20, "bold")
 fontNormal = ("Arial", 10)
 
-#create canvas windows
-canvas1 = Canvas(root, width=800, height=800)
-canvas2 = Canvas(root, width=800, height=800)
-canvas3 = Canvas(root, width=800, height=800)
-helpCanvas = Canvas(root, width=800, height=800)
+#cycles
+functionID = 0 #initialise variable
+startTime = time.time() #initialise time
+elapsedTime = int(time.time() - startTime)
 
-#create canvas buttons
-canvas1.bind("<Button-1>", showCanvas2) #make canvas button
-canvas2.bind("<Button-1>", showCanvas3)
-canvas3.bind("<Button-1>", lambda event: root.quit()) #close window
-helpCanvas.bind("<Button-1>", showCanvas1)
-
-#help button
-helpButton = Button(canvas1, text="Help", command=showHelpCanvas, relief=FLAT)
-helpIcon = PhotoImage(file='assets/icons/question.png')
-helpButton.config(image=helpIcon)
-
-#create a point
-point = canvas2.create_oval(380, 380, 400, 400, fill="black")
-direction = 'left' #add direction
+#threading jazz
+t1 = threading.Thread(target=cardMonitor, daemon=True)
+stopThread = False #initialise flag
 
 showCanvas1() #show main screen
-movePoint() #move point
 
-#Threading jazz
-threading.Thread(target=cardMonitor, daemon=True).start()
-
-root.mainloop()
+root.mainloop() #run gui
