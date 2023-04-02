b26c9914eb8185c7311bb681e5f19a8142ee80f6
elroy.D <72175303+elroydo@users.noreply.github.com>
2023-04-02 16:51:11 +0100

    refactoring & optimisation
     - organised code
     - removed unnecessary elements
     - created helper functions
     - revised comments
     - added image attributions

--- pendle/main.py
+++ pendle/main.py
@@ -1,4 +1,3 @@
-# Import libraries
 import sys
 import time
 from tkinter import BOTH, FLAT, Button, Canvas, PhotoImage, TclError, Tk, messagebox
@@ -6,175 +5,115 @@ from tkinter import BOTH, FLAT, Button, Canvas, PhotoImage, TclError, Tk, messag
 # Import package
 from packages.physiology import Physiology
 
-# GUI jazz
-# # ball moves left to right repeatedly
+# Ball alternates laterally
 def move_ball():
-    global function_ID, start_time, direction # Access global variables
-
-    elapsed_time = int(time.time() - start_time) # Timer
+    global function_ID, start_time, direction
+    # Timer
+    elapsed_time = int(time.time() - start_time)
+    move_distance = 12
+    left_limit, right_limit = 30, 770
+    # Move ball from left to right
     if direction == 1:
-        session_screen.move(ball, -12, 0) # Move ball 10 pixels to the left
-        if session_screen.coords(ball)[0] < 30: # Check coordinates
-            direction = 0 # Left
+        session_screen.move(ball, -move_distance, 0)
+        if session_screen.coords(ball)[0] < left_limit:
+            direction = 0
     else:
-        session_screen.move(ball, 12, 0) # Move ball 10 pixels to the right
-        if session_screen.coords(ball)[2] > 770:
-            direction = 1 # Right
-
-    if elapsed_time > 0 and elapsed_time % 30 == 0: # Show message every 30 seconds
-        session_screen.create_text(400, 300, text="Please take a deep breath", font=font_heading, fill="black", tags="feedback") # Create text
-    if elapsed_time % 40 >= 33: # Delete message after 3 seconds
-        session_screen.delete("feedback") # Removes old text
-    
-    if elapsed_time >= 300: # 5 minute timer
-        pendle.after_cancel(function_ID) # Stop function
-        show_post_screen() # Display next screen
+        session_screen.move(ball, move_distance, 0)
+        if session_screen.coords(ball)[2] > right_limit:
+            direction = 1
+    # Show message every 30 seconds and delete after 3 seconds
+    if elapsed_time > 0 and elapsed_time % 30 == 0:
+        session_screen.create_text(400, 300, text="Please take a deep breath", font=font_heading, fill="black", tags="feedback")
+    elif elapsed_time % 40 >= 33:
+        session_screen.delete("feedback")
+    # 5 minute timer
+    if elapsed_time >= 300:
+        pendle.after_cancel(function_ID)
+        show_post_screen()
     else:
-        function_ID = session_screen.after(10, move_ball) # Update canvas every 10ms
+        # Update canvas every 10ms
+        function_ID = session_screen.after(10, move_ball)
         
+# Toggle canvases
+def toggle_canvas(show_canvas, *hide_canvases):
+    for canvas in hide_canvases:
+        canvas.pack_forget()
+    show_canvas.pack(fill=BOTH, expand=True)
+
+# Display pre-session timer
 def countdown_timer(time_left):
     if time_left > 0:
-        waiting_screen.delete("time") # Remove old text
-        waiting_screen.create_text(400, 50, text=f"Session starts in...{str(time_left)}", font=font_sub_heading2, fill="black", tags="time") # Display time left
-        pendle.after(1000, countdown_timer, time_left-1) # Update countdown after 1 second
+        waiting_screen.delete("time")
+        waiting_screen.create_text(400, 50, text=f"Session starts in...{str(time_left)}", font=font_sub_heading2, fill="black", tags="time")
+        pendle.after(1000, countdown_timer, time_left-1)
 
 # Confirmation dialogue
 def confirm_end_session(event=None):
     result = messagebox.askyesno("Please confirm", "Are you certain you wish to end your session?")
     if result == True:
-        show_post_screen() # Show Post screen
+        show_post_screen()
     else:
-        pass # Stay on the same canvas
+        pass
 
-# Toggle canvases
-def toggle_canvas(canvas, prev_canvas=None):
-    if prev_canvas:
-        prev_canvas.pack_forget()
-    if canvas.winfo_viewable():
-        canvas.pack_forget()
-    else:
-        canvas.pack()
-        
 # Exit application
 def on_closing():
     try:
         pendle.destroy()
     except TclError:
         sys.exit()
-    
+
 # Screen canvases
 def show_start_screen(event=None):
     global monitor
-    
-    monitor = Physiology() # Create thread instance
-    monitor.start() # Start thread
-    
-    help_screen.pack_forget() # Hide element
-    end_screen.pack_forget()
-    
-    start_screen.create_text(400, 400, text="Tap here to start", font=font_heading, fill="black")
-    start_screen.create_text(400, 750, text="Please get comfortable and ensure your camera can clearly see you", font=font_sub_heading2, fill="black")
-    helpButton.pack()
-    helpButton.place(x=750, y=750) # Button position on canvas
-    start_screen.pack(fill=BOTH, expand= True) # Show element
-    
+    # Create and start thread instance
+    monitor = Physiology() 
+    monitor.start()
+    toggle_canvas(start_screen, help_screen, end_screen)
+
 def show_waiting_screen(event=None):
-    start_screen.pack_forget()
-    helpButton.pack_forget()
     countdown_timer(10)
-    waiting_screen.create_text(400, 400, text="Please stay focused and follow the moving object and instructions on your screen",font=font_heading, fill="black", justify="center", width=700)
-    waiting_screen.create_text(400, 750, text="End your 5-minute session at any time by tapping anywhere on the screen", font=font_sub_heading2, fill="black")
-    waiting_screen.pack(fill=BOTH, expand= True)
+    toggle_canvas(waiting_screen, start_screen)
     pendle.after(10000, show_session_screen) # Wait 10 seconds
 
 def show_session_screen(event=None):
     global start_time
-
-    start_time = time.time() # Set start time
-    monitor.toggle_session() # Session in progress
-
-    waiting_screen.pack_forget()
-    move_ball() # Call function to move ball
-    session_screen.pack(fill=BOTH, expand= True)
+     # Set start time and session in progress
+    start_time = time.time()
+    monitor.toggle_session()
+    move_ball()
+    toggle_canvas(session_screen, waiting_screen)
 
 def show_post_screen(event=None):
-    monitor.toggle_session() # Session no longer in progress
-
-    pendle.after_cancel(function_ID) # Stop function
-
-    session_screen.pack_forget()
-    post_session_screen.create_text(400, 400, text="Did you happen to notice anything?", font=font_heading, fill="black")
-    post_session_screen.create_text(400, 750, text="Tap here to proceed", font=font_sub_heading, fill="black")
-    post_session_screen.pack(fill=BOTH, expand= True)
+     # Set Session inactive and stop function
+    monitor.toggle_session()
+    pendle.after_cancel(function_ID)
+    toggle_canvas(post_session_screen, session_screen)
 
 def show_end_screen(event=None):
-    monitor.stop() # Stop thread
-    monitor.join() # Allow thread to completely terminate
-    
-    post_session_screen.pack_forget()
-    end_screen.create_text(400, 400, text="Tap here to close", font=font_heading, fill="black")
-    restartButton.pack(fill=BOTH, expand= True)
-    restartButton.place(x=750, y=750)
-    end_screen.pack()
-    
+     # Stop thread allowing it to completely terminate
+    monitor.stop()
+    monitor.join()
+    toggle_canvas(end_screen, post_session_screen)
+
 def show_help_screen(event=None):
-    start_screen.pack_forget()
-    helpButton.pack_forget()
-    help_screen.create_text(400, 50, text="Pendle Support", font=font_heading, fill="black")
-    # Main topic
-    help_screen.create_rectangle(50, 100, 750, 350, fill="# f5f5f7", outline="# f5f5f7")
-    help_screen.create_text(400, 130, text="Your 5-Minute relaxation session", font=font_sub_heading, fill="black")
-    help_screen.create_text(400, 230, text="Pendle uses a swinging ball, inspired by Eye-Movement Desensitisation and Reprocessing (EMDR) therapy, to help you process your experiences while feeling calm and relaxed.", 
-                           font=font_sub_heading2, fill="black", justify="center", width=600)
-    # Topic two
-    help_screen.create_rectangle(50, 370, 390, 700, fill="# f5f5f7", outline="# f5f5f7")
-    help_screen.create_text(220, 400, text="Monitoring your body", font=font_sub_heading, fill="black")
-    help_screen.create_text(220, 530, text="During the session, Pendle will monitor and record your heart rate, breathing, and emotions.", 
-                           font=font_sub_heading2, fill="black", justify="center", width=300)
-    # Topic three
-    help_screen.create_rectangle(410, 370, 750, 700, fill="# f5f5f7", outline="# f5f5f7")
-    help_screen.create_text(580, 400, text="Giving you feedback", font=font_sub_heading, fill="black")
-    help_screen.create_text(580, 530, text="Occasionally, you will see on-screen instructions to follow, like taking a deep breath.", 
-                           font=font_sub_heading2, fill="black", justify="center", width=300)
-    help_screen.create_text(400, 750, text="Tap here to go back", font=font_sub_heading, fill="black")
-    help_screen.pack(fill=BOTH, expand= True)
-    
-# Create frame instance
+    toggle_canvas(help_screen, start_screen)
+
+# Create fixed size frame instance
 pendle = Tk()
 pendle.title("Pendle")
-pendle.geometry('800x800') # Set geometry
-pendle.resizable(False, False) # Set fixed window size
-pendle.protocol("WM_DELETE_WINDOW", on_closing) # Destroy instance upon closing
+pendle.geometry('800x800')
+pendle.resizable(False, False)
+pendle.protocol("WM_DELETE_WINDOW", on_closing)
 
 # Create canvas windows
-screen_height = 800
-screen_width = 800
-start_screen = Canvas(pendle, width=screen_width, height=screen_height) # Start
-waiting_screen = Canvas(pendle, width=screen_width, height=screen_height) # Waiting
-session_screen = Canvas(pendle, width=screen_width, height=screen_height) # Session
-post_session_screen = Canvas(pendle, width=screen_width, height=screen_height) # Post-session
-end_screen = Canvas(pendle, width=screen_width, height=screen_height) # End
-help_screen = Canvas(pendle, width=screen_width, height=screen_height) # Help
-
-# Create canvas buttons
-start_screen.bind("<Button-1>", show_waiting_screen) # To Waiting screen 
-session_screen.bind("<Button-1>", confirm_end_session) # To Post-session screen 
-post_session_screen.bind("<Button-1>", show_end_screen) # To End screen 
-help_screen.bind("<Button-1>", lambda event: toggle_canvas(start_screen, help_screen)) # To Start screen 
-end_screen.bind("<Button-1>", lambda event: pendle.quit()) # Quit application 
-
-# Buttons
-help_icon = PhotoImage(file='./assets/images/question.png') # Import help icon
-restart_icon = PhotoImage(file='./assets/images/restart.png') # Import restart icon
-helpButton = Button(start_screen, text="Help", command=show_help_screen, image=help_icon, relief=FLAT) # Create help button
-restartButton = Button(end_screen, text="Restart", command=show_start_screen, image=restart_icon, relief=FLAT) # Create restart button
-
-# Create ball
-x = screen_width//2 # Calculate x centre
-y = screen_height//2 # Calculate y centre
-radius = 10 # Set radius
-ball = session_screen.create_oval(x - radius, y - radius, x + radius, y + radius, fill="black") # Create ball
-direction = 0 # Add direction
+height = 800
+width = 800
+start_screen = Canvas(pendle, width=width, height=height) # Start
+waiting_screen = Canvas(pendle, width=width, height=height) # Pre-session
+session_screen = Canvas(pendle, width=width, height=height) # Session
+post_session_screen = Canvas(pendle, width=width, height=height) # Post-session
+end_screen = Canvas(pendle, width=width, height=height) # End
+help_screen = Canvas(pendle, width=width, height=height) # Help
 
 # Create font styles
 font_heading = ("Arial", 28, "bold")
@@ -182,13 +121,62 @@ font_sub_heading = ("Arial", 16, "bold")
 font_sub_heading2 = ("Arial", 14)
 font_normal = ("Arial", 10)
 
-# Cycles
-function_ID = 0 # Initialise variable
-start_time = time.time() # Initialise time
-elapsed_time = int(time.time() - start_time)  # Initialise elapsed time
+# Import icons
+help_icon = PhotoImage(file='./assets/images/question.png')
+restart_icon = PhotoImage(file='./assets/images/restart.png')
 
-# Threading jazz
-monitor = Physiology()
+# Create buttons and bindings
+start_screen.bind("<Button-1>", show_waiting_screen) # To Waiting screen
+session_screen.bind("<Button-1>", confirm_end_session) # To Post-session screen
+post_session_screen.bind("<Button-1>", show_end_screen) # To End screen
+help_screen.bind("<Button-1>", lambda event: toggle_canvas(start_screen, help_screen)) # To Start screen
+end_screen.bind("<Button-1>", lambda event: pendle.quit()) # Quit application
+
+helpButton = Button(start_screen, text="Help", command=show_help_screen, image=help_icon, relief=FLAT)
+restartButton = Button(end_screen, text="Restart", command=show_start_screen, image=restart_icon, relief=FLAT)
+
+helpButton.pack()
+helpButton.place(x=750, y=750) # Position on canvas
+restartButton.pack(fill=BOTH, expand=True)
+restartButton.place(x=750, y=750)
+
+# Create shapes
+x = width//2 # Calculate x centre
+y = height//2 # Calculate y centre
+radius = 10
+ball = session_screen.create_oval(x - radius, y - radius, x + radius, y + radius, fill="black")
+
+help_screen.create_rectangle(50, 100, 750, 350, fill="#f5f5f7", outline="#f5f5f7")
+help_screen.create_rectangle(50, 370, 390, 700, fill="#f5f5f7", outline="#f5f5f7")
+help_screen.create_rectangle(410, 370, 750, 700, fill="#f5f5f7", outline="#f5f5f7")
+
+# Create text elements
+start_screen.create_text(400, 400, text="Tap here to start", font=font_heading, fill="black")
+start_screen.create_text(400, 750, text="Please get comfortable and ensure your camera can clearly see you", font=font_sub_heading2, fill="black")
+
+waiting_screen.create_text(400, 400, text="Please stay focused and follow the moving object and instructions on your screen", font=font_heading, fill="black", justify="center", width=700)
+waiting_screen.create_text(400, 750, text="End your 5-minute session at any time by tapping anywhere on the screen", font=font_sub_heading2, fill="black")
+
+help_screen.create_text(400, 50, text="Pendle Support", font=font_heading, fill="black")
+help_screen.create_text(400, 130, text="Your 5-Minute relaxation session", font=font_sub_heading, fill="black")
+help_screen.create_text(400, 230, text="Pendle uses a swinging ball, inspired by Eye-Movement Desensitisation and Reprocessing (EMDR) therapy, to help you process your experiences while feeling calm and relaxed.", font=font_sub_heading2, fill="black", justify="center", width=600)
+help_screen.create_text(220, 400, text="Monitoring your body", font=font_sub_heading, fill="black")
+help_screen.create_text(220, 530, text="During the session, Pendle will monitor and record your heart rate, breathing, and emotions.", font=font_sub_heading2, fill="black", justify="center", width=300)
+help_screen.create_text(580, 400, text="Giving you feedback", font=font_sub_heading, fill="black")
+help_screen.create_text(580, 530, text="Occasionally, you will see on-screen instructions to follow, like taking a deep breath.", font=font_sub_heading2, fill="black", justify="center", width=300)
+help_screen.create_text(400, 750, text="Tap here to go back", font=font_sub_heading, fill="black")
+help_screen.create_text(400, 785, text="Question & Reload  icons created by Freepik - Flaticon", font=font_normal, fill="#dddddd")
+
+post_session_screen.create_text(400, 400, text="Did you happen to notice anything?", font=font_heading, fill="black")
+post_session_screen.create_text(400, 750, text="Tap here to proceed", font=font_sub_heading, fill="black")
+
+end_screen.create_text(400, 400, text="Tap here to close", font=font_heading, fill="black")
+
+# Session parameters
+function_ID = 0
+start_time = time.time()
+elapsed_time = int(time.time() - start_time)
+direction = 0 # Add direction
 
 # Show start screen
 show_start_screen()
--- pendle/packages/physiology.py
+++ pendle/packages/physiology.py
@@ -2,8 +2,8 @@ import time
 import threading
 import numpy as np
 import cv2 as cv
-from deepface import DeepFace
 
+# Import packages
 from .resp_rate import calculate_brpm
 from .analyse_emotion import get_dominant_emotion
 from .plot_utils import plot_signals
@@ -17,14 +17,14 @@ class Physiology(threading.Thread):
         self.db = PendleDatabase()
         self.csv_handler = CSVHandler()
         self.session = False
-    
+
     # Stop thread
     def stop(self):
-        self.stop_event.set() # Sets event
+        self.stop_event.set()
 
     # Toggle session
     def toggle_session(self):
-        self.session^=True # Switches between true and false
+        self.session ^= True
 
     def run(self):
         print('Monitoring...')
@@ -42,44 +42,44 @@ class Physiology(threading.Thread):
         heart_index = 0
         heart_cache_size = 30
         heart_cache = np.zeros((heart_cache_size))
-        
+
         # Gaussian pyramid variables
         face_box = 200
         ROI_gauss = np.zeros((cache_size, face_box//8, face_box//8, 3))
 
-        # Bandpass filter for specific frequencies
-        freqs = (1.0*framerate) * np.arange(cache_size) / (1.0 * cache_size) # Create array of evenly spaced frequency values
-        
-        # # Create boolean array to filter Hz - Sample heart rates in the range of 50-120 bpm
-        filter = (freqs >= 0.8) & (freqs <= 2) 
+        # Bandpass filter for specific frequencies - Create array of evenly spaced frequency values
+        freqs = (1.0*framerate) * np.arange(cache_size) / (1.0 * cache_size)
+
+        # Create boolean array to filter Hz - Sample heart rates in the range of 50-120 bpm
+        filter = (freqs >= 0.8) & (freqs <= 2)
 
         # Data metrics
         heart_rate = []
         respiration_rate = []
         emotions = []
         session_active = []
-        
+
         # Frame data
         bpm = 0
         brpm = 0
         dominant_emotion = ''
-        
+
         # Miscellaneous
         last_time = time.time()
 
         # Check if camera is accessible
         if not cap.isOpened():
             print("Camera inaccessible...")
-        
+
         # Check stopping thread condition
         while not self.stop_event.is_set():
             # Timing variables
             current_time = time.time()
             elapsed_time = current_time - last_time
-            
+
             # Capture each frame
             check, frame = cap.read()
-            
+
             # Set check to true if frame is read correctly
             if not check:
                 print("Missing frames, ending capture...")
@@ -87,65 +87,67 @@ class Physiology(threading.Thread):
 
             # Pre-processing; enhance frames
             gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            
+
             # Face detection parameters
             face = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
 
             if len(face) > 0:
-                (x, y, w, h) = face[0] # Extract face coordinates            
+                # Extract face coordinates
+                (x, y, w, h) = face[0]
 
-                # Heart rate and breathing
                 # Calculate centre and fixed size box around face
-                x1, y1 = max(x + w // 2 - face_box // 2, 0), max(y + h // 2 - face_box // 2, 0)
+                x1, y1 = max(x + w // 2 - face_box // 2,0), max(y + h // 2 - face_box // 2, 0)
                 x2, y2 = min(x1 + face_box, frame.shape[1]), min(y1 + face_box, frame.shape[0])
 
                 # Extract ROI from frame
                 face_ROI = frame[y1:y2, x1:x2]
 
                 # Apply gaussian pyramid function to ROI (scaled down image 3 levels)
-                down_ROI = face_ROI # Temporary holder
+                down_ROI = face_ROI
                 for _ in range(3):
-                    down_ROI = cv.pyrDown(down_ROI) # Downscale frame 
+                    down_ROI = cv.pyrDown(down_ROI)
 
-                # Add downscaled gauss frame to index in list
+                # Add downscaled frame to index in list
                 try:
                     ROI_gauss[index] = down_ROI
                 except Exception as e:
                     print(e)
 
-                # Apply Fourier tranform function to downscaled gauss frame 
-                fourier = np.fft.fft(ROI_gauss, axis=0) # Compute frequency spectrum along time domain
+                # Apply Fourier tranform function to downscaled frame
+                fourier = np.fft.fft(ROI_gauss, axis=0)
 
-                # Apply bandpass filter 
-                fourier[filter == False] = 0 # Keep specific frequencies
+                # Apply bandpass filter keeping specific frequencies
+                fourier[filter == False] = 0
 
-                # Grab heart and respiration rates 
+                # Grab heart and respiration rates
                 if index % framerate//2 == 0: # Sampling rate
-                    fourier_average = np.real(fourier).mean(axis=(1, 2, 3)) # Store signal averages
-                    heart_cache[heart_index] = 60.0 * freqs[np.argmax(fourier_average)] # Multiply max values (hertz) by 60 (seconds)
-                    heart_index = (heart_index + 1) % heart_cache_size # Increment circular index
+                     # Compute & store signal averages from top-down
+                    fourier_average = np.real(fourier).mean(axis=(1, 2, 3))
+                    # Multiply corresponding frequency (Hertz) by 60 (seconds)
+                    heart_cache[heart_index] = 60.0 * freqs[np.argmax(fourier_average)]
+                    # Increment circular index
+                    heart_index = (heart_index + 1) % heart_cache_size
+                 # Increment circular index
+                index = (index + 1) % cache_size
 
-                index = (index + 1) % cache_size # Increment circular index
-                
-                # Estimate emotions, heart rate, and breathing very second
+                # Calculate & store emotions, heart rate, and breathing every second
                 if elapsed_time >= 1.0:
-                    # Calculate metrics and add to lists
-                    bpm = heart_cache.mean() # Average of bpms in heart values cache 
-                    brpm = calculate_brpm(bpm) # Ratio heartbeats to breathing
-                    dominant_emotion = get_dominant_emotion(frame)
-                    
+                    bpm = heart_cache.mean() # Heart rate
+                    brpm = calculate_brpm(bpm) # Respiration rate
+                    dominant_emotion = get_dominant_emotion(frame) # Emotion
+
                     # Add data to lists
-                    heart_rate.append(bpm) # Add bpm to list
-                    respiration_rate.append(brpm) # Add brpm to list
-                    emotions.append(dominant_emotion) # Add emotion to list
-                    session_active.append(self.session) # Add session state to list
-                    
+                    heart_rate.append(bpm)
+                    respiration_rate.append(brpm) 
+                    emotions.append(dominant_emotion)
+                    session_active.append(self.session) # Session state
+
                     # Reset time
                     last_time = current_time
 
-                # Show ROI in window
+                # Show ROI in window; demo
                 try:
-                    frame[90:90+25, 10:10+25] = down_ROI # Demo
+                    frame[90:90+25, 10:10+25] = down_ROI 
                     frame[120:120+face_box, 10:10+face_box] = face_ROI * 170
                     frame[y1:y2, x1:x2] = cv.pyrUp(cv.pyrUp(cv.pyrUp(down_ROI))) * 170
                 except Exception as e:
@@ -159,15 +161,13 @@ class Physiology(threading.Thread):
             if frame is not None:
                 cv.imshow('Janus', frame)
                 cv.waitKey(1)
-        
-        # Zip data making it iterable
+
+        # Zip & save iterable data
         data = list(zip(heart_rate, respiration_rate, emotions, session_active))
-        # Save data to database
         self.db.add_data(data)
-        # Save data to CSV file
         self.csv_handler.save_data(data)
-        
+
         # Clean up
-        cv.destroyAllWindows() # Close all windows
-        cap.release() # Close camera capture
+        cv.destroyAllWindows()
+        cap.release()
         print("Terminating...")
